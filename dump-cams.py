51be7545b4f59e66e46c0b075d64e93449791a7b
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-02-17 17:35:10 +0100

    Add files via upload

---
+++ dump-cams.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import requests, re , colorama
+colorama.init()
+
+
+try:
+    print()
+    countries = ["NL"]
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+
+    num = int(input("Write \033[34m1 \033[31mto \033[34mDump>>> "))
+    if num not in range(1, 91+1):
+        raise IndexError
+
+    country = countries[num-1]
+    res = requests.get(
+        f"https://www.insecam.org/en/bycountry/{country}", headers=headers
+    )
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+
+    for page in range(int(last_page)):
+        res = requests.get(
+            f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
+            headers=headers
+        )
+        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
+            print("\033[1;31m", ip)
+except:
+    pass
+finally:
+    print("\033[1;37m")
+    exit()
---
+++ dutchcams.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# Get random cameras from Netherland and connect to them =)
+# Not legal. We are not responsable
+# Python3
+# Coded by z3ntl3 root (efdal)
+
+#Modules
+import os, re , requests , colorama , sys
+colorama.init()
+#EndModules
+
+#Panel DESIGN
+print("""
+
+                           \033[36m\033[1m╔╦╗╦ ╦╔╦╗╔═╗╦ ╦  ╔═╗╔═╗╔╦╗╔═╗  
+                            ║║║ ║ ║ ║  ╠═╣  ║  ╠═╣║║║╚═╗\033[36m\033[1m\033[0m
+                           \033[34m═╩╝╚═╝ ╩ ╚═╝╩ ╩  ╚═╝╩ ╩╩ ╩╚═╝\033[34m
+                            \033[31m--\033[31m\033[33m>\033[33m \033[31mz3ntl3 root\033[31m \033[34m(EFDAL)\033[34m \033[33m<\033[33m\033[31m--\033[31m
+                             \033[34m╔══════════════════════╗\033[34m
+                             \033[34m║\033[34m \033[31mx\033[31m \033[36mLegit Word:\033[36m \033[32mDUMP\033[32m \033[31mx\033[31m \033[34m║\033[34m
+                             \033[34m╚══════════════════════╝\033[34m
+                               \033[1m\033[37m╔══╗
+                               ╚╗╔╝
+                               ╔╝(¯`v´¯)
+                               ╚══`.¸.Efdal (z3ntl3 root)\033[1m\033[37m\033[0m
+
+
+""")
+#EndPanel
+
+
+dumpword = input("\033[1m\033[36m\033[34m╔═\033[34m\033[31m[\033[31m\033[33m~\033[33m\033[31m]\033[31m\033[36mr00t\033[36m\033[33m@\033[33m\033[35mEfdal\033[35m\033[33m[\033[33m\033[36m\033[36m\033[36mDutch-Cams\033[33m]\033[33m\033[32m$\033[32m\033[36m\033[1m\033[0m\n\033[1m\033[34m╚═══════►\033[34m\033[1m\033[0m")
+while True:
+    if dumpword == "DUMP":
+        break
+    if dumpword == "dump":
+        break
+    else:
+        os.system('clear')
+        print("Invalid choice\n>>>Bye..")
+        print("\nPrevious you entered invalid word. Write & enter the legit word!")
+        os.system('python3 dutchcams.py')
+        sys.exit()
+
+#Beginning of software tool z3ntl3 root (efdal) xd
+os.system('clear')
+print("\033[31mDumping NL Cams...\n\033[36m WAIT xd\033[0m")
+dumpaces = input("\033[31mPress \033[33menter\033[31m to \033[34mstart\033[34m \033[33mdumping xd\033[31m")
+
+executeTOOL = "python3 dump-cams.py"
+os.system(executeTOOL)
+dumpNLcamsRANDOM = "1"
+os.system(dumpNLcamsRANDOM)
+os.system('1')
+
+
+
